94eae88b941721654d2d247a4f1de50770d8e74d
LAPTOP-I1Q52PFT\13220 <1322023401@qq.com>
2017-04-28 16:02:48 +0800

    新增专家投票和普通投票

--- modules/voteNumber.js
+++ modules/voteNumber.js
@@ -6,7 +6,15 @@ exports.vote = function (info) {
     return db.transaction(function (trx) {
         return co(function *() {
             //增加投票数目
-            yield voteNumberModel.vote(trx, info.teacherId);
+            let increment = {
+                'number_of_votes': voteNumberModel.raw(trx, 'number_of_votes + 1')
+            };
+            if((info.code + '')[0] == 1) {
+                increment['expert_number'] = voteNumberModel.raw(trx, 'expert_number + 1');
+            } else {
+                increment['ordinary_number'] = voteNumberModel.raw(trx, 'ordinary_number + 1');
+            }
+             yield voteNumberModel.vote(trx, info.teacherId, increment);
             //更新邀请码状态
             yield invitationCodeModel.update(trx, {invitation_code: info.code}, 1, info.teacherId.toString());
         }).then(trx.commit).catch(trx.rollback);
